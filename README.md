0e63736a8996a803c876a0d53691b77b79a8e9c8
RAYDAN GASPAR <raydangaspar@gmail.com>
2020-05-25 19:32:46 -0300

    Factory

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Design Patterns 2
 
 Curso Alura Design Patterns Python II: Boas práticas de programação
+
+Prerequisites:
+
+```sudo apt-get install python3```
+
+```sudo apt-get install python3-pip```
+
+```pip install mysqlclient```
---
+++ connection_factory.py
@@ -0,0 +1,14 @@
+import MySQLdb
+
+
+class ConnectionFactory:
+
+    def get_connection(self):
+        connection = MySQLdb.connect(
+            host="localhost",
+            user='root',
+            passwd='',
+            db='alura'
+        )
+
+        return connection
